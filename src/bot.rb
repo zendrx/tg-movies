3e188f3ff49e307b2b9328dfcd23a2c9953f06b9
ZenDrx <ynwghosted@icloud.com>
2026-06-29 08:29:05 +0100

    Update bot.rb

--- src/bot.rb
+++ src/bot.rb
@@ -1,6 +1,7 @@
 # bot.rb
 require 'telegem'
 require 'httparty'
+require 'webrick'
 require_relative 'start'
 require_relative 'search'
 require_relative 'watch'
@@ -16,5 +17,15 @@ Handlers::Watch.register(bot)
 Handlers::Account.register(bot)
 Handlers::Admin.register(bot)
 Handlers::Help.register(bot)
+
+Thread.new do
+port = ENV['PORT'] || '3000'
+server = WEBrick::HTTPServer.new(Port: port, Logger: WEBrick::Log.new("/dev/null"), AccessLog: [])
+server.mount_proc '/' do |req, res|
+ res.content_type = 'text/plain'
+ res.body = 'TGMovies Bot is running'
+end
+server.start
+end
 
 bot.start_polling
