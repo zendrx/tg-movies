2fb79740a4a9de2da608043d4de12bdd23898f2d
ZenDrx <ynwghosted@icloud.com>
2026-06-29 17:43:21 +0100

    Update start.rb

--- src/start.rb
+++ src/start.rb
@@ -1,6 +1,8 @@
 # start.rb
 module Handlers
   module Start
+    GROUP_ID = ENV["GROUP_ID"]
+
     def self.register(bot)
       bot.command('start') do |ctx|
         welcome_text = <<~TEXT
@@ -21,6 +23,39 @@ module Handlers
           row "❓ Help"
         end
         keyboard.resize
+
+        # Check if user was referred
+        referrer_id = ctx.command_args&.strip
+
+        if referrer_id && referrer_id.match?(/^\d+$/)
+          referrer_id = referrer_id.to_i
+
+          # Don't self-refer
+          if referrer_id != ctx.from.id
+            ctx.session[:referred_by] = referrer_id
+
+            # Send notification to group with usernames
+            if GROUP_ID
+              begin
+                new_user = ctx.from
+                new_user_mention = new_user.username ? "@#{new_user.username}" : "[#{new_user.first_name}](tg://user?id=#{new_user.id})"
+
+                bot.api.call('sendMessage', {
+                  chat_id: GROUP_ID,
+                  text: "🎉 *New Referral!*\n\n" \
+                        "👤 *New User:* #{new_user_mention}\n" \
+                        "🆔 *New User ID:* `#{new_user.id}`\n\n" \
+                        "🔗 *Referred By ID:* `#{referrer_id}`\n\n" \
+                        "💰 Admin can reward referrer!",
+                  parse_mode: "Markdown",
+                  disable_web_page_preview: true
+                })
+              rescue => e
+                ctx.logger&.error("Failed to send referral notification: #{e.message}")
+              end
+            end
+          end
+        end
 
         ctx.photo(
           "https://tomoviestv.netlify.app/img/welcome.jpg",
